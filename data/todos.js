126535ecb827757827d6629dea74a06d3b23be8e
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2022-02-13 21:37:12 -0300

    static-data ℹ

---
+++ data/todos.js
@@ -0,0 +1,39 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Take out the trash",
+    isCompleted: true,
+    isToday: true,
+    hour: "08:00",
+  },
+  {
+    id: 2,
+    text: "Tomarse un café",
+    isCompleted: false,
+    isToday: false,
+    hour: "20:00",
+  },
+  {
+    id: 3,
+    text: "Desayunar",
+    isCompleted: true,
+    isToday: true,
+    hour: "180:00",
+  },
+  {
+    id: 4,
+    text: "Respirar",
+    isCompleted: false,
+    isToday: true,
+    hour: "11:00",
+  },
+  {
+    id: 5,
+    text: "Limpiar la casa",
+    isCompleted: true,
+    isToday: true,
+    hour: "10:00",
+  },
+]
+
+export default todosData;
